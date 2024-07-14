a37dc0d73def11c30bfb3c3725d357aef644ce97
Thomas <awtpianka@gmail.com>
2024-07-14 23:24:40 +0200

    Compile data for group 1 vaults and simplify logo image path

---
+++ app/lib/apparatus-data.js
@@ -0,0 +1,151 @@
+const vaultData = [
+  {
+    name: "Front Handspring Group",
+    url: "front-handspring",
+    apparatus: "vault",
+    alts: ["Kim"],
+    members: [
+      {
+        name: "Front handspring",
+        difficulty: 1.60
+      },
+      {
+        name: "Front handspring on, 1/2 turn off",
+        difficulty: 2.00
+      },
+      {
+        name: "Front handspring on, 1 full turn off",
+        difficulty: 2.60
+      },
+      {
+        name: "Front handspring on, 1 1/2 turn off",
+        difficulty: 3.20,
+        alt: ["Kim"]
+      },
+      {
+        name: "Front handspring on, 2 full turns off",
+        difficulty: 3.60
+      },
+      {
+        name: "Front handspring on, 2 1/2 turns off",
+        difficulty: 4.00
+      }
+    ]
+  },
+  {
+    name: "Yamashita Group",
+    url: "yamashita",
+    apparatus: "vault",
+    members: [
+      {
+        name: "Yamashita",
+        difficulty: 2.00
+      },
+      {
+        name: "Yamashita with 1/2 turn off",
+        difficulty: 2.40
+      },
+      {
+        name: "Yamashita with 1 full turn off",
+        difficulty: 2.80
+      }
+    ]
+  },
+  {
+    name: "Front Handspring with 1/2 Turn Group",
+    url: "front-handspring-half-turn",
+    apparatus: "vault",
+    members: [
+      {
+        name: "Front handspring with 1/2 turn on, repulsion off",
+        difficulty: 1.60
+      },
+      {
+        name: "Front handspring with 1/2 turn on, 1/2 turn off",
+        difficulty: 2.40
+      },
+      {
+        name: "Front handspring with 1/2 turn on, 1 full turn off",
+        difficulty: 2.60
+      },
+      {
+        name: "Front handspring with 1/2 turn on, 1 1/2 turn off",
+        difficulty: 3.20
+      },
+      {
+        name: "Front handspring with 1/2 turn on, 2 full turns off",
+        difficulty: 3.60
+      }
+    ]
+  },
+  {
+    name: "Front Handspring with 1 Full Turn Group",
+    url: "front-handspring-full-turn",
+    apparatus: "vault",
+    alts: ["Korbut"],
+    members: [
+      {
+        name: "Front handspring with 1 full turn on, front handspring off",
+        difficulty: 3.20
+      },
+      {
+        name: "Front handspring with 1 full turn on, 1 full turn off",
+        difficulty: 3.60,
+        alt: ["Korbut"]
+      }
+    ]
+  },
+  {
+    name: "Round-off, Flic-Flac Group",
+    url: "round-off-flic-flac",
+    apparatus: "vault",
+    members: [
+      {
+        name: "Round-off, flic-flac on, repulsion off",
+        difficulty: 2.00
+      }
+    ]
+  },
+  {
+    name: "Round-off, Flic-Flac with 1/2 Turn Group",
+    url: "round-off-flic-flac-half-turn",
+    apparatus: "vault",
+    members: [
+      {
+        name: "Round-off, flic-flac with 1/2 turn on, front handspring off",
+        difficulty: 2.20
+      },
+      {
+        name: "Round-off, flic-flac with 1/2 turn on, 1/2 turn off",
+        difficulty: 2.60
+      },
+      {
+        name: "Round-off, flic-flac with 1/2 turn on, 1 full turn off",
+        difficulty: 3.00
+      },
+      {
+        name: "Round-off, flic-flac with 1/2 turn on, 1 1/2 turn off",
+        difficulty: 3.40
+      }
+    ]
+  },
+  {
+    name: "Round-off, Flic-Flac with 1 Full Turn Group",
+    url: "round-off-flic-flac-full-turn",
+    apparatus: "vault",
+    members: [
+      {
+        name: "Round-off, flic-flac with 1 full turn on, repulsion off",
+        difficulty: 2.40
+      },
+      {
+        name: "Round-off, flic-flac with 1 full turn on, 1/2 turn off",
+        difficulty: 2.80
+      },
+      {
+        name: "Round-off, flic-flac with 1 full turn on, 1 full turn off",
+        difficulty: 3.20
+      }
+    ]
+  }
+];
--- app/ui/logo.tsx
+++ app/ui/logo.tsx
@@ -1,11 +1,10 @@
 import Image from "next/image";
-import logo from "@/public/logo.png"
 
 export default function Logo() {
   return (
     <div className="flex flex-row invert-[98%] drop-shadow-[0_2px_2px_rgb(0,0,0)]">
       <Image 
-        src={logo}
+        src="/logo.png"
         width={64}
         height={64}
         alt="Logo image of mid-air gymnast inside a medal" 
